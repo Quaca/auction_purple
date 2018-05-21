99f2d8a7d916d939789e8d419043b6c8b636c7d8
Quaca <amar.kvakic@gmail.com>
2018-05-21 12:52:32 +0200

    Added static landing-page

--- frontend/app/components/feature-collection.js
+++ frontend/app/components/feature-collection.js
@@ -1,6 +1,9 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+<<<<<<< 27c22a56dc4a50813e90368857a859e7409be507
     tagName: 'article',
     classNames:['border-2 hover-effect']
+=======
+>>>>>>> Added static landing-page
 });
