b960e14c022738ee71d217503417bd5c123699c8
Quaca <amar.kvakic@gmail.com>
2018-05-21 12:52:32 +0200

    Added static landing-page

--- frontend/app/controllers/index.js
+++ frontend/app/controllers/index.js
@@ -1,4 +1,5 @@
 import Controller from '@ember/controller';
+<<<<<<< 27c22a56dc4a50813e90368857a859e7409be507
 import Ember from 'ember';
 import { alias } from '@ember/object/computed'
 //import {service as itemService} from '../services/item-service';
@@ -16,3 +17,48 @@ export default Controller.extend({
 
     
 });
+=======
+
+export default Controller.extend({
+
+    model1: [
+        {
+            name:"Shoes",
+            startPrice: 35,
+            imgURL: "assets/images/shoes.png"
+        },
+        {
+            name:"Minions",
+            startPrice: 10,
+            imgURL: "assets/images/min.jpg"
+        },
+        {
+            name:"Patika",
+            startPrice:25,
+            imgURL: "assets/images/patika.jpg"
+        }
+    ],
+    model2: [
+        {
+            name:"Car-Porsche",
+            startPrice: 100000,
+            imgURL: "assets/images/porse.jpg"
+        },
+        {
+            name:"Minions",
+            startPrice: 10,
+            imgURL: "assets/images/min.jpg"
+        },
+        {
+            name:"Ball for soccer",
+            startPrice:30,
+            imgURL: "assets/images/lopta.jpg"
+        },
+        {
+            name:"Desktop computer",
+            startPrice: 500,
+            imgURL: "assets/images/komp.png"
+        }
+    ]
+});
+>>>>>>> Added static landing-page
