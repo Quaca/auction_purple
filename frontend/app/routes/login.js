51590eb695f47d382d91bb62371d8357c18347da
Quaca <amar.kvakic@gmail.com>
2018-06-02 15:17:07 +0200

    Removed conf and some changes

--- frontend/app/routes/login.js
+++ frontend/app/routes/login.js
@@ -3,14 +3,10 @@ import Route from '@ember/routing/route';
 export default Route.extend({
 
     resetController(controller, isExiting, transition) {
-        //empty by default
-        //fires when route changes or model is refreshed
-        // isExiting property true when exiting (obviously)
-
         if(isExiting){
             controller.set('errorMessage', null);
             controller.set('email', null);
             controller.set('password', null);
         }
-        }
+    }
 });
--- frontend/app/routes/register.js
+++ frontend/app/routes/register.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    resetController(controller, isExiting, transition) {
+        if(isExiting){
+            controller.set('errorMessage', null);
+            controller.set('email', null);
+            controller.set('password', null);
+            controller.set('firstName', null);
+            controller.set('lastName', null);
+            
+        }
+    }
 });
