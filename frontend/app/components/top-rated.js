fd4a5fa7e20c8b6c40d7f343192493beb45fee43
Quaca <amar.kvakic@gmail.com>
2018-06-03 11:20:50 +0200

    login2(2)

--- frontend/app/components/top-rated.js
+++ frontend/app/components/top-rated.js
@@ -46,49 +46,5 @@ export default Component.extend({
             
     },
 
-    model1: [
-        {
-                    name:"Shoes",
-                    startPrice: 35,
-                    imgURL: "assets/images/shoes.png"
-                },
-                {
-                    name:"Minions",
-                    startPrice: 10,
-                    imgURL: "assets/images/min.jpg"
-                },
-                {
-                    name:"Patika",
-                    startPrice:25,
-                    imgURL: "assets/images/patika.jpg"
-                },
-                {
-                    name:"Shoes",
-                    startPrice: 35,
-                    imgURL: "assets/images/shoes.png"
-                }
-    ],
-    model2: [
-        {
-            name:"Car-Porsche",
-            startPrice: 100000,
-            imgURL: "assets/images/porse.jpg"
-        },
-        {
-            name:"Minions",
-            startPrice: 10,
-            imgURL: "assets/images/min.jpg"
-        },
-        {
-            name:"Ball for soccer",
-            startPrice:30,
-            imgURL: "assets/images/lopta.jpg"
-        },
-        {
-            name:"Desktop computer",
-            startPrice: 500,
-            imgURL: "assets/images/komp.png"
-        }
-    ]
 
 });
