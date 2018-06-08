2fd001635a3b089e4b6c9a5f70a70270e283ef61
Quaca <amar.kvakic@gmail.com>
2018-06-08 10:07:48 +0200

    last-commit

--- frontend/app/components/page-footer.js
+++ frontend/app/components/page-footer.js
@@ -30,7 +30,9 @@ export default Component.extend({
             )
             .catch(
                 (error) => {
-                    this.set('errorMessage', error.responseText)
+                    this.set('errorMessage', error.responseText);
+                    this.set('successMessage', null);
+                    
                 }
             );
         }
